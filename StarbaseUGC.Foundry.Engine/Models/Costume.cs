8c1ced26be323d3c96b34fd5af37813601b9de28
Aaron <negeth@Hotmail.com>
2019-03-12 21:05:08 -0700

    Start costumes

--- StarbaseUGC.Foundry.Engine/Models/Costume.cs
+++ StarbaseUGC.Foundry.Engine/Models/Costume.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StarbaseUGC.Foundry.Engine.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace StarbaseUGC.Foundry.Engine.Models
 {
     public class Costume : FoundryObject
     {
+        public string Description { get { return GetFieldValue(Constants.Costume.Description); } }
+        public string DisplayName { get { return GetFieldValue(Constants.Costume.DisplayName); } }
     }
 }
