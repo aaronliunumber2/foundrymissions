1d72f3f5b6e3adbb4f80cf64b3a21fd9278637ae
Aaron <negeth@Hotmail.com>
2019-04-09 20:31:18 -0700

    more work on dialogue section

--- FoundryMissionsCom/Scripts/viewdata.js
+++ FoundryMissionsCom/Scripts/viewdata.js
@@ -145,6 +145,7 @@ function doDialogs(dialogs) {
     }
 }
 
+var displayNameLength = 30;
 function getDialogDisplayName(dialog) {
     var dialogName = dialog.VisibleName;
 
@@ -159,7 +160,7 @@ function getDialogDisplayName(dialog) {
         promptBody = promptBody.replace(/\\n/g, " ");
 
         //show the first 20 characters
-        dialogName = promptBody.substring(0, 20);
+        dialogName = promptBody.substring(0, displayNameLength);
 
         //if it ends with &> remove it
         if (dialogName.endsWith(dialogEndChars)) {
@@ -183,7 +184,7 @@ function getDialogTabPageHtml(dialog) {
     var id = getDialogId(dialog);
     var html = "<div id='"+ id +"' class='tab-pane'>";
     //do its own dialog
-    html += "<div>" + dialog.PromptBody + "</div>";
+    html += getDialogPromptDiv(dialog);
 
     //now add all the next prompts (if there are any)
     var prompts = dialog.DialogPrompts;
@@ -191,10 +192,79 @@ function getDialogTabPageHtml(dialog) {
     for (j = 0; j < prompts.length; j++) {
 
         var prompt = prompts[j];
-        html += "<div>" + prompt.PromptBody + "</div>";
+        html += getDialogPromptDiv(prompt);
     }
     
 
     html += "</div>";
     return html;
+}
+
+function getDialogPromptDiv(prompt) {
+    /*prompt contains these things
+      1. Costume
+      2. Animation
+      3. Title
+      4. Text
+      5. Actions (buttons)
+    */
+
+    var costume = prompt.PromptCostume;
+    var animation = formatPromptStyle(prompt.PromptStyle);
+    var title = formatPromptTitle(prompt.PromptTitle);
+    var text = formatPromptBody(prompt.PromptBody);
+    var actions = prompt.Action;
+
+    var promptHtml = "<div class='prompt'>";
+    promptHtml += "<div class='promptBody'>";
+    promptHtml += "<div>Costume: " + costume + "</div>";
+    promptHtml += "<div>Title: " + title + "</div>";
+    promptHtml += "<div>Text</div>";
+    promptHtml += "<div class='promptText'>" + text + "</div>";
+    promptHtml += "</div>"; //promptbody
+    promptHtml += getActionHtml(actions);
+    promptHtml += "</div>"; //prompt
+
+
+    return promptHtml;
+}
+
+function getActionHtml(actions) {
+    return "";
+}
+
+function formatPromptStyle(style){
+    if (!style){
+        return "[Default]";
+    }
+    else{
+        return style;
+    }
+}
+
+function formatPromptTitle(title){
+    if (!title){
+        return "";
+    }
+    else{
+        return title;
+    }
+}
+
+function formatPromptBody(promptBody) {
+    /*formatting 
+      1. gets rid of <& and &> from the beginnings
+      2. gets rid of " from the beginning and ending
+      3. replaces \n with <br>
+    */
+    if (promptBody.startsWith(dialogStartChars) && promptBody.endsWith(dialogEndChars)) {
+        promptBody = promptBody.substring(2, promptBody.length - 2);
+    }
+    else if (promptBody.startsWith('"') && promptBody.endsWith('"')) {
+        promptBody = promptBody.substring(1, promptBody.length - 1);
+    }
+
+    promptBody = promptBody.replace(/\\n/g, "<br>");
+
+    return promptBody;
 }
